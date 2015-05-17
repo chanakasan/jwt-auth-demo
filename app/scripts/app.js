8ecfb4aacb2e2013a69a40b71de6ab64688105ff
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-05-17 15:05:18 +0530

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jwtAppApp
+ * @description
+ * # jwtAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jwtAppApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jwtAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jwtAppApp
+ */
+angular.module('jwtAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
